20246b1a5aa1cae7d14bac926aea1f4db8aa49a8
cesar puentes <caps2596@gmail.com>
2021-09-22 15:56:02 -0500

    🚧 uso y manejo de inquirer

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 require('colors')
 
-const { mostrarMenu, pausa } = require('./helpers/mensajes')
+const { inquirerMenu, pausa } = require('./helpers/inquirer')
 
 console.clear()
 const main = async () => {
-  mostrarMenu()
-  pausa()
+  let opt = ''
+
+  do {
+    opt = await inquirerMenu()
+    if (opt !== '0') await pausa()
+  } while (opt !== '0')
 }
 
 main()
--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,38 +1,45 @@
+const { resolve } = require('path')
+
 require('colors')
 
 const mostrarMenu = () => {
-  console.clear()
-  console.log('========================='.green)
-  console.log('Seleccione una opcion'.green)
-  console.log('=========================\n'.green)
+  return new Promise(resolve => {
+    console.clear()
+    console.log('========================='.green)
+    console.log('Seleccione una opcion'.green)
+    console.log('=========================\n'.green)
 
-  console.log(`${'1.'.green} Crear tarea`)
-  console.log(`${'2.'.green} Listar tareas`)
-  console.log(`${'3.'.green} Listar tareas completadas`)
-  console.log(`${'4.'.green} Listar tareas pendientes`)
-  console.log(`${'5.'.green} Completar tareas`)
-  console.log(`${'5.'.green} Borrar tarea`)
-  console.log(`${'0.'.green} Salir\n`)
+    console.log(`${'1.'.green} Crear tarea`)
+    console.log(`${'2.'.green} Listar tareas`)
+    console.log(`${'3.'.green} Listar tareas completadas`)
+    console.log(`${'4.'.green} Listar tareas pendientes`)
+    console.log(`${'5.'.green} Completar tareas`)
+    console.log(`${'5.'.green} Borrar tarea`)
+    console.log(`${'0.'.green} Salir\n`)
 
-  const leerLinea = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  })
+    const leerLinea = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    })
 
-  leerLinea.question('Seleccione una opcion: ', opt => {
-    console.log(opt)
-    leerLinea.close()
+    leerLinea.question('Seleccione una opcion: ', opt => {
+      leerLinea.close()
+      resolve(opt)
+    })
   })
 }
 
 const pausa = () => {
-  const leerLinea = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  })
+  return new Promise(resolve => {
+    const leerLinea = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    })
 
-  leerLinea.question(`\nPresione ${'Enter'.green} para continuar\n`, opt => {
-    leerLinea.close()
+    leerLinea.question(`\nPresione ${'Enter'.green} para continuar\n`, opt => {
+      leerLinea.close()
+      resolve()
+    })
   })
 }
 
